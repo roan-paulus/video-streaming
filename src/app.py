04460b6bdb5168696f1b6365b0e7692e4241441a
roan-paulus <roanpaulus@proton.me>
2026-02-08 20:31:19 +0100

    a basic video player for my files

---
+++ src/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, url_for
+
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    video_list_url = url_for("video_list")
+    return render_template('root.html', video_list_url=video_list_url)
+
+@app.route("/video-list")
+def video_list():
+    BASE_PATH = 'src/static/'
+    paths = [
+        os.path.join(dirpath, f).replace(BASE_PATH, "")
+        for (dirpath, dirnames, filenames) in os.walk(BASE_PATH)
+        for f in filenames
+    ]
+    return render_template('video_list.html', paths=paths)
+
+@app.route("/videos/<filepath>")
+def video(filepath=None):
+    filepath = os.path.normpath(filepath)
+    name, ext = os.path.splitext(filepath)
+
+    # if len(basename) == 2:
+    #     file_name = p[0]
+    #     extension = p[1]
+    # else:
+    #     raise Exception(f"'{p}' has no extention")
+    static_path = url_for('static', filename=f'videos/{filepath}')
+    codec = ext[1:]
+    return render_template('video_player.html', filepath=static_path, codec=codec, ext=ext)
+
